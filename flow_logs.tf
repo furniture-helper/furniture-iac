0b85ffb892961c966e4976d67ed1cb30d702bfd1
kaneeldias <kaneeldias@hotmail.com>
2026-01-03 11:26:18 +0530

    Refactor OPA policy check step and add tags to flow logs resources

--- flow_logs.tf
+++ flow_logs.tf
@@ -42,7 +42,7 @@ resource "aws_iam_role_policy" "flow_logs_policy" {
         "logs:DescribeLogGroups",
         "logs:DescribeLogStreams"
       ]
-      Resource = aws_cloudwatch_log_group.vpc_flow_logs.arn
+      Resource = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
     }]
   })
 }
