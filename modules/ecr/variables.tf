e3f69c7d6ab73d974f88478547c533a9aee99301
Kaneel Dias <kaneeldias@hotmail.com>
2026-01-03 12:28:40 +0530

    Organize Terraform configuration by renaming files and adding modules for IAM roles, ECR, and VPC (#4)

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,7 @@
+variable "project" {
+  type = string
+}
+
+variable "ecr_repository_name" {
+  type = string
+}
---
+++ modules/iam_roles/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "github_organization" {
+  type = string
+}
+variable "ecr_repository_name" {
+  type = string
+}
