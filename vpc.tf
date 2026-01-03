5600b13a85b2da4ae951cc46a200a694eb9a8b0c
kaneeldias <kaneeldias@hotmail.com>
2026-01-03 09:20:29 +0530

    Add self and cidr_blocks to VPC security group egress rules

--- vpc.tf
+++ vpc.tf
@@ -28,12 +28,14 @@ resource "aws_default_security_group" "default_sg" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
+    self = true
   }
 
   egress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
