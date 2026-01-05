eec8588efa69778588a79852e2cc66b1d04bbabf
kaneeldias <kaneeldias@hotmail.com>
2026-01-05 11:37:11 +0530

    Update S3 bucket names for Terraform backend and crawler storage

--- modules/ecs/logs.tf
+++ modules/ecs/logs.tf
@@ -15,15 +15,9 @@ locals {
   derived_ecs_task_execution_role_name = length(var.ecs_task_execution_role_arn) > 0 ? split("/", var.ecs_task_execution_role_arn)[1] : ""
 }
 
-data "aws_iam_role" "exec" {
-  count = local.derived_ecs_task_execution_role_name == "" ? 0 : 1
-  name  = local.derived_ecs_task_execution_role_name
-}
-
 resource "aws_iam_role_policy" "ecs_logs_policy" {
-  count = local.derived_ecs_task_execution_role_name == "" ? 0 : 1
-  name  = "${local.derived_ecs_task_execution_role_name}-ecs-logs"
-  role  = data.aws_iam_role.exec[0].name
+  name = "${local.derived_ecs_task_execution_role_name}-ecs-logs"
+  role = local.derived_ecs_task_execution_role_name
 
   policy = jsonencode({
     Version = "2012-10-17"
