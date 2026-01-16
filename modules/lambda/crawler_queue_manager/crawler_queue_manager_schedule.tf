a2dbd69abd01b6a6f5b105606c5ef881dbadf495
Kaneel Dias <kaneeldias@hotmail.com>
2026-01-17 00:33:25 +0530

    Add CloudWatch event rule to trigger crawler queue manager Lambda fun… (#21)
    * Add CloudWatch event rule to trigger crawler queue manager Lambda function hourly
    
    * Add tags to CloudWatch event rule for crawler queue manager Lambda function

---
+++ modules/lambda/crawler_queue_manager/crawler_queue_manager_schedule.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_event_rule" "crawler_queue_manager_hourly" {
+  name                = "${var.project}-crawler-queue-manager-hourly"
+  description         = "Triggers lambda function every hour"
+  schedule_expression = "cron(0 * * * ? *)"
+
+  tags = {
+    Project = var.project
+    Name    = "${var.project}-crawler-queue-manager-hourly-event-rule"
+  }
+}
+
+resource "aws_cloudwatch_event_target" "check_every_hour" {
+  rule      = aws_cloudwatch_event_rule.crawler_queue_manager_hourly.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.crawler_queue_manager_lambda_function.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.crawler_queue_manager_lambda_function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.crawler_queue_manager_hourly.arn
+}
